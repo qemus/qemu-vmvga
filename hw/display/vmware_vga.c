0cdd7412b908677a1941b96f132eebe6263962bb
Kroese <kroese@users.noreply.github.com>
2026-08-25 14:03:56 +0200

    Update and rename vmware_vga.c21 to vmware_vga.c

--- hw/display/vmware_vga.c
+++ hw/display/vmware_vga.c
@@ -7,6 +7,8 @@
  Copyright (c) 2023-2026 Christopher Eric Lentocha
  <christopherericlentocha@gmail.com>
 
+ Copyright (c) 2026 https://github.com/qemus/qemu-vmvga
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights

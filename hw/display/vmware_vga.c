46ddc689e37258b4dd31ab70810f08f89b218ab8
Kroese <kroese@users.noreply.github.com>
2026-09-07 14:46:03 +0200

    Add files via upload

--- hw/display/vmware_vga.c
+++ hw/display/vmware_vga.c
@@ -33,7 +33,6 @@
 #include "qapi/error.h"
 #include "qemu/main-loop.h"
 #include "exec/target_page.h"
-#include "system/ram_addr.h"
 #include "trace.h"
 #include "include/vmware_vga_compat.h"
 #include "include/vmware_vga_gmr.h"
@@ -1734,9 +1733,8 @@ static inline void vmsvga_mark_vram_dirty_rect(
      */
     dirty_log_mask = memory_region_get_dirty_log_mask(&s->vga.vram);
     dirty_log_mask &= (uint8_t)~(1U << DIRTY_MEMORY_VGA);
-    cpu_physical_memory_set_dirty_range(
-        memory_region_get_ram_addr(&s->vga.vram) + (ram_addr_t)start,
-        (ram_addr_t)span, dirty_log_mask);
+    vmvga_memory_region_set_dirty_mask(&s->vga.vram, (hwaddr)start,
+                                       (hwaddr)span, dirty_log_mask);
 }
 
 static inline void vmsvga_mark_active_rect_dirty(
